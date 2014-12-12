cc2a4286138028ff8d663df349e40c9b72566b2b
Cristi <cristi.catea@gmail.com>
2014-12-11 16:47:18 -0800

    update 1

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,3 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
+console.log('Event Page for Browser Action');
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,60 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+$('#save').on("click", function () {
+	var name = $('#name');
+	var details = $('#details');
+	var time = new Date().getTime();
+
+	if (name.val().length === '' || name.val().length === 0) {
+		alert('Enter a name');
+	} else {
+		var obj = {};
+		obj[time] = {name: name.val(), details: details.val()};
+		chrome.storage.sync.set(obj, function () {
+			$('#alert').show();
+			name.val('');
+			details.val('');
+			setTimeout(function () {
+				$('#alert').fadeOut();
+			}, 1000);
+		});
+	}
+
+	return false;
+});
+
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+	if ($(e.target).attr('href') === '#view') getData();
+});
+
+getData();
+
+function getData() {
+	chrome.storage.sync.get(function (data) {
+		var htm = '';
+		for (var key in data) {
+			var obj = data[key];
+			htm += getColapsable(key, obj);
+		}
+		$('#accordion').html(htm);
+	});
+}
+
+function getColapsable(key, obj) {
+	var d = new Date(key * 1000);
+	console.log(d.toDateString());
+
+	var html = '';
+	html += '<div class="panel panel-default">';
+	html += '   <div class="panel-heading" role="tab" id="heading' + key + ' ' + d.toDateString() + '">';
+	html += '       <h4 class="panel-title">';
+	html += '           <a data-toggle="collapse" data-parent="#accordion" href="#collapse' + key + '" aria-expanded="true" aria-controls="collapse' + key + '">' + obj.name + '</a>';
+	html += '       </h4>';
+	html += '   </div>';
+	html += '   <div id="collapse' + key + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading' + key + '">';
+	html += '       <div class="panel-body">' + obj.details + '</div>';
+	html += '   </div>';
+	html += '</div>';
+
+	return html;
+}
