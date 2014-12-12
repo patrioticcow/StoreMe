05e03f71f4c7cf500625cdc1c2b2661938a2e949
Cristi <cristi.catea@gmail.com>
2014-12-12 11:04:01 -0800

    0.3.5

--- scripts/popup.js
+++ scripts/popup.js
@@ -23,8 +23,49 @@ $('#save').on("click", function () {
 	return false;
 });
 
+$('#import').on("click", function () {
+	var importContent = $('#import_content');
+
+	if (importContent.val().length === '' || importContent.val().length === 0) {
+		alert('Nothing to import');
+	} else {
+		console.log('start');
+		chrome.storage.sync.get(function (data) {
+			for (var key in data) {
+				chrome.storage.sync.remove(key);
+			}
+		});
+
+		chrome.storage.sync.set(JSON.parse(importContent.val()), function () {
+			$('#import_alert').show();
+			setTimeout(function () {
+				$('#import_alert').fadeOut();
+			}, 1000);
+		});
+	}
+
+	return false;
+});
+
+$('.store_me').on("click", '.remove', function (e) {
+	e.preventDefault();
+	var key = $(this).data('key').toString();
+	$('#panel_' + key).remove();
+	chrome.storage.sync.remove(key);
+	return false;
+});
+
 $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
 	if ($(e.target).attr('href') === '#view') getData();
+	if ($(e.target).attr('href') === '#export') {
+		chrome.storage.sync.get(function (data) {
+			$('#export_content').val(JSON.stringify(data));
+		});
+	}
+});
+
+$('#accordion').on('show.bs.collapse', function (e) {
+	chrome.storage.sync.set({active_tab : $(e.target).attr('id')});
 });
 
 getData();
@@ -32,11 +73,20 @@ getData();
 function getData() {
 	chrome.storage.sync.get(function (data) {
 		var htm = '';
+		var collapseId;
 		for (var key in data) {
 			var obj = data[key];
-			htm += getColapsable(key, obj);
+			if(key !== 'active_tab') htm += getColapsable(key, obj);
+			if(key === 'active_tab') collapseId = obj;
 		}
+
 		$('#accordion').html(htm);
+
+		$('#' + collapseId).collapse('show');
+
+		$('#search').hideseek({
+			highlight: true
+		});
 	});
 }
 
@@ -45,10 +95,13 @@ function getColapsable(key, obj) {
 	console.log(d.toDateString());
 
 	var html = '';
-	html += '<div class="panel panel-default">';
-	html += '   <div class="panel-heading" role="tab" id="heading' + key + ' ' + d.toDateString() + '">';
+	html += '<div class="panel panel-default" id="panel_' + key + '">';
+	html += '   <div class="panel-heading" role="tab" id="heading' + key + '">';
 	html += '       <h4 class="panel-title">';
-	html += '           <a data-toggle="collapse" data-parent="#accordion" href="#collapse' + key + '" aria-expanded="true" aria-controls="collapse' + key + '">' + obj.name + '</a>';
+	html += '           <a data-toggle="collapse" data-parent="#accordion" href="#collapse' + key + '" aria-expanded="true" aria-controls="collapse' + key + '">';
+	html += '               ' + obj.name + ' <small class="pull-right">' + d.toDateString() + ' ';
+	html += '               <button class="btn btn-xs btn-danger remove" data-key="' + key + '"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button></small>';
+	html += '           </a>';
 	html += '       </h4>';
 	html += '   </div>';
 	html += '   <div id="collapse' + key + '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading' + key + '">';
